d7fb1d8d66abd2eca1b744a04fe311b8843cd512
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-10 14:57:00 +0000

    Added zeckendorf function at script.js

--- js/script.js
+++ js/script.js
@@ -10,9 +10,29 @@ function getNumberInfo() {
     } else {
         txt += `You have entered the number ${num}. <p>`;
         txt += `Zeckendorf Number Representation of ${num} is ${zeckendorf(num)}`;
-        txt += `${zeckendorf(num)}`
     }
 
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
+}
+
+// Function to Get the Zeckendorf Number Representation
+function zeckendorf(n) {
+    if (n==0) return n.toString();
+    let fib = [2,1];
+    while (fib[0]<n) {
+        fib.unshift(fib[0]+fib[1]);
+    }
+    let dig = [];
+    fib.forEach(function(f) {
+        if (f<=n) {
+            dig = dig + [1];
+            n = n - f;
+        } else {
+            dig += [0];
+        }
+    });
+
+    // If the first digit is 1 return all the digits, otherwise remove the zero
+    return dig[0]==1 ? dig : dig.slice(1);
 }
